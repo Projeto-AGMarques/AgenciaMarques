71e96b7ca252153a1eeae8b2659099586e6a498a
ArthurB0rges <arthurtutu1987@gmail.com>
2026-06-05 01:38:18 -0300

    Atualizações

--- global/renderGlobal.js
+++ global/renderGlobal.js
@@ -1,6 +1,7 @@
 import {Screen_AboutLocal} from "../pages/about_local/index.js"
 import {Screen_FAQ} from "../pages/faq/index.js"
 import {accordionFAQ} from "../pages/faq/accordionAction.js"
+import {Screen_AboutCompany} from "../pages/about_company/index.js"   /* <-- DANDO ERRO SÓ DE IMPORTAR. */
 
 
 //#region Variáveis do Index.html
@@ -12,6 +13,7 @@ let FAQ = document.getElementById("faq");
 //#endregion
 
 home.innerHTML = Screen_AboutLocal;
+aboutCompany.innerHTML = Screen_AboutCompany;
 FAQ.innerHTML = Screen_FAQ;
 
 accordionFAQ();
