41e38e7ca9bb0091f393152a608a3ff012be7525
ArthurB0rges <arthurtutu1987@gmail.com>
2026-06-15 02:37:09 -0300

    Finalização

--- global/renderGlobal.js
+++ global/renderGlobal.js
@@ -3,6 +3,7 @@ import {Screen_AboutLocal} from "../pages/about_local/index.js"
 import {Screen_FAQ} from "../pages/faq/index.js"
 import {Screen_AboutCompany} from "../pages/about_company/index.js"
 import { Screen_Produtos, initGlider } from "../pages/products_screen/index.js"; /* <--- Import */ 
+import { Screen_Home } from "../pages/home/index.js";
 /* #endregion */
 
 /* #region Componentes */
@@ -12,7 +13,7 @@ import { header, itensSlideHeader, initAccessibilityMenu } from "../components/h
 /* #endregion */
 
 //#region Variáveis do Index.html
-let body = document.querySelector("body");
+let body = document.querySelector('body');
 let home = document.getElementById("home");
 let productsScreen = document.getElementById("products_screen");
 let aboutCompany = document.getElementById("about_company");
@@ -20,13 +21,15 @@ let aboutLocal = document.getElementById("about_local");
 let FAQ = document.getElementById("faq");
 //#endregion
 
-home.insertAdjacentHTML("beforebegin", header)
-home.insertAdjacentHTML("beforebegin", itensSlideHeader)
+home.insertAdjacentHTML("beforebegin", header);
+home.insertAdjacentHTML("beforebegin", itensSlideHeader);
+home.innerHTML = Screen_Home;
 aboutLocal.innerHTML = Screen_AboutLocal;
 aboutCompany.innerHTML = Screen_AboutCompany;
 productsScreen.innerHTML = Screen_Produtos; /* <--- Import */
 FAQ.innerHTML = Screen_FAQ;
-FAQ.insertAdjacentHTML("afterend", Component_Footer);
+body.insertAdjacentHTML("afterend", Component_Footer);
+// FAQ.insertAdjacentHTML("afterend", Component_Footer);
 
 accordionFAQ();
 initGlider();
---
+++ pages/home/index.js
@@ -0,0 +1,93 @@
+
+
+export const Screen_Home = `
+    <main class="home">
+
+        <section class="hero-home" id="hero">
+            <div class="container">
+                <div class="text-hero-home">
+                    <h1 class="letring-home">
+                        O melhor lugar para <br />
+                        se comprar é aqui!
+                    </h1>
+                    <p>--- Marques Magazini ---</p>
+                </div>
+                
+            </div>
+        </section>
+
+        <section class="products-bar-home">
+
+            <h2 class="letring-products-home">Produtos em Destaque</h2>
+
+            <div class="bar-home">
+                
+                <div class="cards-home">
+                    <img src="../../global/assets/imgsHome/ventilador.jpg" alt="" />
+                    <h3>Ventilador</h3>
+                    <a target="_blank" href="https://wa.me/5519994581963?text=Ol%C3%A1!%20Tenho%20interesse%20em%20comprar%20na%20Agência%20Marques." 
+                    class="button_products_home">Entrar em contato</a>
+                </div>
+
+                <div class="cards-home">
+                    <img src="../../global/assets/imgsHome/baixados (5).webp" alt="" />
+                    <h3>Chaleira</h3>
+                    <a target="_blank" href="https://wa.me/5519994581963?text=Ol%C3%A1!%20Tenho%20interesse%20em%20comprar%20na%20Agência%20Marques." 
+                    class="button_products_home">Entrar em contato</a>
+                </div>
+                <div class="cards-home">
+                    <img src="../../global/assets/imgsHome/baixados (4).webp" alt="" />
+                    <h3>Batedeira</h3>
+                    <a target="_blank" href="https://wa.me/5519994581963?text=Ol%C3%A1!%20Tenho%20interesse%20em%20comprar%20na%20Agência%20Marques." 
+                    class="button_products_home">Entrar em contato</a>
+                </div>
+                <div class="cards-home">
+                    <img src="../../global/assets/imgsHome/baixados (3).webp" alt="" />
+                    <h3>Airfryer</h3>
+                    <a target="_blank" href="https://wa.me/5519994581963?text=Ol%C3%A1!%20Tenho%20interesse%20em%20comprar%20na%20Agência%20Marques." 
+                    class="button_products_home">Entrar em contato</a>
+                </div>
+                <div class="cards-home">
+                    <img src="../../global/assets/imgsHome/OIP (4).webp" alt="" />
+                    <h3>Pinceis</h3>
+                    <a target="_blank" href="https://wa.me/5519994581963?text=Ol%C3%A1!%20Tenho%20interesse%20em%20comprar%20na%20Agência%20Marques." 
+                    class="button_products_home">Entrar em contato</a>
+                </div>
+            </div>
+        </section>
+
+        <section class="exposition-products-home">
+            <div class="int-circle">
+                <span class="circle-left"><img src="../../global/assets/imgsHome/Lustre.png" alt="" /></span>
+            </div>
+            <div class="content-exposition-home-0">
+                <h2>Qualidade que você merece, pelo preço que você procura.</h2>
+
+                <p>
+                    Na Marques Magazin você encontra os melhores produtos para sua casa,
+                    com qualidade, preço justo e atendimento diferenciado.
+                </p>
+            </div>
+            <div class="int-circle">
+                <span class="circle-right">
+                    <img src="../../global/assets/imgsHome/Maquina_de_lavar.png" alt="" />
+                </span>
+            </div>
+            <div class="content-exposition-home-1">
+                <h3>
+                    Milhares de clientes já escolheram a qualidade Marques Magazin.
+                </h3>
+
+                <p>
+                    Qualidade que conquista clientes todos os dias. Encontre tudo o que
+                    você precisa para sua casa com o melhor custo-benefício.
+                </p>
+            </div>
+
+            <div class="int-circle">
+                <span class="circle-small-right"></span>
+            </div>
+        </section>
+
+    </main>
+`
